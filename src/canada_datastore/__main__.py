af5489017fcbe61e49a81181b7696932f74d9cf6
Jose Gómez-Dans <jose.gomez-dans@kcl.ac.uk>
2023-07-21 16:32:11 +0000

    FIRMS downloader added to script. Needs testing

--- src/canada_datastore/__main__.py
+++ src/canada_datastore/__main__.py
@@ -1,6 +1,7 @@
 import click
 from canada_datastore import select_product_filter
 from canada_datastore import get_s3_lvl2_products
+from canada_datastore import get_firms_date
 import datetime as dt
 import logging
 from pathlib import Path
@@ -24,15 +25,20 @@ PICKLE_LAKE = [
 @click.option(
     "-f2", "--lvl2folder", default="./", help="Level2 product folder"
 )
+@click.option(
+    "-ff", "--firmsfolder", default="./", help="FIRMS product folder"
+)
 @click.option("-d", "--date", default=None, help="Level 1 start date")
-def main(lvl1folder, lvl2folder, date):
+def main(lvl1folder, lvl2folder, firmsfolder, date):
     lvl1folder = Path(lvl1folder)
     lvl2folder = Path(lvl2folder)
+    firmsfolder = Path(firmsfolder)
     if not lvl1folder.exists():
         raise IOError("Can't find lvl1 product folder")
     if not lvl2folder.exists():
         raise IOError("Can't find lvl2 product folder")
-
+    if not firmsfolder.exists():
+        raise IOError("Can't find FIRMS folder")
     get_s3_lvl2_products(lvl2folder.as_posix())
     if date is not None:
         try:
@@ -41,6 +47,7 @@ def main(lvl1folder, lvl2folder, date):
             logger.error("Dates should be given as YYYY-MM-DD format")
     else:
         start_day = dt.datetime(2023, 7, 15)
+    _ = get_firms_date(start_day, output_folder=firmsfolder, range=2)
     day1 = dt.timedelta(days=1)
     today = start_day
     while today <= dt.datetime.now():
