625874d2ad42de597a57fad7ac4dbd818d24be7c
Jose Gómez-Dans <jose.gomez-dans@kcl.ac.uk>
2023-07-21 16:33:35 +0000

    fix typo

--- src/canada_datastore/__init__.py
+++ src/canada_datastore/__init__.py
@@ -3,7 +3,7 @@ import sys
 
 from .sentinel3_dloader import select_product_filter  # noqa
 from .sentinel3_lvl2 import get_s3_lvl2_products  # noqa
-from .get_firms import get_firms  # noqa
+from .get_firms import get_firms_date  # noqa
 
 logging.basicConfig(
     level=logging.INFO,
